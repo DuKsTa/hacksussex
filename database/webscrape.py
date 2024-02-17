32cf7aebbad7493384b16409f616d383ccf2ffb9
Mohamed Lafi <mohamedlafi@Mohameds-MacBook-Air.local>
2024-02-17 18:29:25 +0000

    Removed gabage code and added ability to scrape questions

--- database/webscrape.py
+++ database/webscrape.py
@@ -16,15 +16,6 @@ driver.get("https://www.hastingsdirect.com/help/")  # Replace with the actual UR
 # Wait for the page to load
 time.sleep(5)
 
-button_xpath = "/html/body/div[1]/div/div/div/div[7]/div[1]"
-# Define the base XPaths for the questions and answers containers
-insurance_type_xpath = "/html/body/div[1]/div/div/div/div[4]/button[1]"
-faq_xpath = "/html/body/div[1]/div/div/div/div[6]/div/button[1]"
-
-home_insurance = "/html/body/div[1]/div/div/div/div[4]/button[2]"
-home_insurance_faq = "/html/body/div[1]/div/div/div/div[6]/div[1]"
-home_questions = "/html/body/div[1]/div/div/div/div[6]/div[2]/div"
-
 # Locate the container that holds all the buttons
 container_xpath = "#__next > div > div > div > div.styles_helpTriageMainButtonContainer__REpR5"
 container = driver.find_elements(By.CSS_SELECTOR, container_xpath)
@@ -32,63 +23,35 @@ container = driver.find_elements(By.CSS_SELECTOR, container_xpath)
 faq = "#__next > div > div > div > div.styles_topFaqWrapper__wCfac > div"
 faqs = driver.find_elements(By.CSS_SELECTOR, faq)
 
-""
-"#__next > div > div > div > div.styles_topFaqWrapper__wCfac > div.styles_topFaqContainer__n8Ij3"
 # questions = "#__next > div > div > div > div.styles_topFaqWrapper__wCfac > div.styles_resultsContainer__mpVop"
 # question = driver.find_element(By.CSS_SELECTOR, questions)
-
+question = ""
 # Main page
-for i in container:
-    # click on the insurances
-    i.click()
-    buttons = driver.find_elements(By.TAG_NAME, "button")
-    for index, b in enumerate(buttons):
-        # If it's the car insurance then skip
-        if index == 0:
-            continue
-        # Select the insurances
-        b.click()
-        # button = driver.find_elements(By.TAG_NAME, "button")
-        # print(f"Length of it is {len(button)}")
-        button = driver.find_elements(By.XPATH, "//*[@id='__next']/div/div/div/div[6]/div[1]/button")
-        for t in button:
-            t.click()
-            questions = driver.find_elements(By.XPATH, "//*[@id='__next']/div/div/div/div[6]/div[2]/div")
-            print(f"Length of divs are {len(questions)}")   
-            for div in questions:
-                buttons = driver.find_elements(By.TAG_NAME, "button")
-                for butt in buttons:
-                    print(div)
-
-        # for t in button:
-        #     t.click()
-
+with open('data_scraping.txt', 'w') as f:
+    for i in container:
+        # click on the insurances
+        i.click()
+        buttons = driver.find_elements(By.TAG_NAME, "button")
+        for index, b in enumerate(buttons):
+            # If it's the car insurance then skip
+            if index == 0:
+                continue
+            # Select the insurances
+            b.click()
+            button = driver.find_elements(By.XPATH, "//*[@id='__next']/div/div/div/div[6]/div[1]/button")
+            for t in button:
+                t.click()
+                time.sleep(1)
+                questions = driver.find_elements(By.XPATH, "//*[@id='__next']/div/div/div/div[6]/div[2]/div//*")
+                time.sleep(0.1)
+                print(f"Length of divs are {len(questions)}")
+                for q in questions:
+                    if len(q.text) < 5:
+                        continue
+                    if question == q.text.strip():
+                        continue
+                    question = q.text.strip()
+                    print(q.accessible_name)
+                    f.write(f'Question:{question}\n')
     
-# for i in container:
-#     print(i)
-
-# Find all the button elements within the container
-# buttons = container.find_elements(By.TAG_NAME, 'button')
-# faq = container.find_elements(By.XPATH, home_insurance_faq)
-
-# # Click each button
-# for button in buttons:
-#     button.click()
-#     for i in faq:
-#         print(i)
-
-# butt = "/html/body/div[1]/div/div/div/div[6]/div[2]/div/div[2]/div/div/p[1]"
-
-# buttons_text = driver.find_element(By.XPATH, butt)
-
-# print(buttons_text.text)
-
-# # Loop through each <p> element and extract the text
-# for p in paragraphs:
-#     print(p)
-#     text = p.text
-#     # Process the text as needed, such as printing or saving to a file
-#     print(text)
-
-# Close the driver when done
 driver.quit()
