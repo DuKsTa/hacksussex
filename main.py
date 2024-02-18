233893bcf1fcd8e4da08f83813413728d3eed52b
Mohamed Lafi <mohamedlafi@Mohameds-MacBook-Air.local>
2024-02-18 08:05:35 +0000

    Merged branches and added the main file

---
+++ main.py
@@ -0,0 +1,8 @@
+from backend.vector_database import Vector_Database
+import sys
+from streamlit.web import cli
+
+
+if __name__ == '__main__':
+    sys.argv = ["streamlit", "run", "frontend/Test.py"]
+    sys.exit(cli.main())
